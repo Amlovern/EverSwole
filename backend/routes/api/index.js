c6a3c16cc890c44109b0f96b24c0152983fb61dc
Anthony Lovern <voltorodarkwatch@gmail.com>
2022-04-28 16:33:19 -0400

    User login api route created and tested.

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -1,6 +1,15 @@
 const router = require('express').Router();
+const sessionRouter = require('./session');
+const usersRouter = require('./users');
 const asyncHandler = require('express-async-handler');
 const { setTokenCookie, restoreUser, requireAuth } = require('../../utils/auth.js');
 const { User } = require('../../db/models');
+
+router.use('/session', sessionRouter);
+router.use('/users', usersRouter);
+
+router.post('/test', (req, res) => {
+    res.json({ requestBody: req.body });
+});
 
 module.exports = router;
