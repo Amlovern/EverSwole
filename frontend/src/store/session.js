226ff6ee3a4b7b521af41c19cbf8291e71569ffc
Anthony Lovern <voltorodarkwatch@gmail.com>
2022-04-29 18:32:16 -0400

    Restore user setup and tested

--- frontend/src/store/session.js
+++ frontend/src/store/session.js
@@ -2,11 +2,17 @@ import { csrfFetch } from "./csrf";
 
 const SET_USER = 'session/SET_USER';
 const REMOVE_USER = 'session/REMOVE_USER';
+const RESTORE_USER = 'session/RESTORE_USER'
 
 const set = (user) => ({
     type: SET_USER,
     user
 });
+
+const restore = (user) => ({
+    type: RESTORE_USER,
+    user
+})
 
 const remove = () => ({
     type: REMOVE_USER,
@@ -32,6 +38,16 @@ export const loginUser = (user) => async dispatch => {
     }
 }
 
+export const restoreUser = () => async dispatch => {
+    const response = await csrfFetch('/api/session');
+
+    if (response.ok) {
+        const loggedUser = await response.json();
+        dispatch(restore(loggedUser.user));
+        return loggedUser;
+    }
+}
+
 /*
     If current session user, session slice of state looks like this:
     {
@@ -53,7 +69,12 @@ const sessionReducer = (state = initialState, action) => {
         case SET_USER:
             const newUserState = {};
             newUserState.user = action.user.user;
+            console.log(action.user);
             return newUserState;
+        case RESTORE_USER:
+            const restoredUserState = {};
+            restoredUserState.user = action.user;
+            return restoredUserState;
         case REMOVE_USER:
             const newState = {...state};
             newState['user'] = null;
