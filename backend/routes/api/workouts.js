5e6e0b72d55e67199820e5977c9846b58774fee2
Anthony Lovern <voltorodarkwatch@gmail.com>
2022-05-04 17:06:34 -0400

    All workout backend routes created and tested

--- backend/routes/api/workouts.js
+++ backend/routes/api/workouts.js
@@ -7,6 +7,17 @@ const { handleValidationErrors } = require('../../utils/validation');
 
 const router = express.Router();
 
+const validateWorkout = [
+    check('title')
+        .exists({ checkFalsy: true })
+        .withMessage('Please provide a title.'),
+    check('title')
+        .exists({ checkFalsy: true })
+        .isLength({ max: 256 })
+        .withMessage('Title must be less than 256 characters.'),
+    handleValidationErrors
+];
+
 router.get('/', restoreUser, asyncHandler(async (req, res) => {
     const { user } = req;
     const workouts = await db.Workout.findAll({
@@ -36,6 +47,41 @@ router.get('/:workoutId', restoreUser, asyncHandler(async (req, res) => {
         console.log('WORKOUT', workout.title)
     } else {
         console.log('Try again next time!')
+    }
+}));
+
+router.post('/', restoreUser, validateWorkout, asyncHandler(async (req, res) => {
+    const { user } = req;
+    const { title } = req.body;
+
+    const validationErrors = validationResult(req);
+
+    if (validationErrors.isEmpty()) {
+        const newWorkout = await db.Workout.create({
+            userId: user.id,
+            title
+        });
+        return res.json(newWorkout)
+    } else {
+        const errors = validationErrors.array().map((error) => error.msg);
+        console.error(errors)
+    }
+}));
+
+router.delete('/:workoutId', restoreUser, asyncHandler(async (req, res) => {
+    const { user } = req;
+    const workoutId = req.params.workoutId;
+
+    const specificWorkout = await db.Workout.findOne({
+        where: {
+            userId: user.id,
+            id: workoutId
+        }
+    })
+
+    if (specificWorkout) {
+        await specificWorkout.destroy();
+        return res.json(specificWorkout);
     }
 }));
 
